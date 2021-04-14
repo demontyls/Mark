2a21e7d71c32070d63141cee5bc906cc9caa0524
Oleg <Snejokkk555@mail.ru>
2021-04-14 18:09:17 +0300

    s
    ss

---
+++ style.js
@@ -0,0 +1,6 @@
+document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('.burger-span').classList.toggle('active');
+  })
+  document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('.heder-list').classList.toggle('wind');
+  })
